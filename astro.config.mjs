b9d42253113094f057fde4f441f4149b13eabd8f
musch038 <musch.sam@gmail.com>
2025-05-17 13:24:20 -0500

    fix: remove redundant sidebar config

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,22 +4,15 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'My Docs',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-		}),
-	],
+  integrations: [
+    starlight({
+      title: 'My Docs',
+      social: [
+        { icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }
+      ],
+
+      // 👇  ONE sidebar option; autogenerate everything
+      // sidebar: { '*': 'autogenerate' },
+    }),
+  ],
 });
